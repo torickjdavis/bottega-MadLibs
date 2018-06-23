d16241bd6253ad027ed2d34355325fe2a9b018a4
Torick Davis <torickjdavis@gmail.com>
2018-06-23 20:50:15 +0000

    Added remaining Inputs, and refactored Input Component.

--- src/components/card.js
+++ src/components/card.js
@@ -7,7 +7,21 @@ class Card extends Component {
 		super();
 		this.state = {
 			color: '',
-			pluralNoun: ''
+			pluralNoun: '',
+			adjectiveOne: '',
+			celebrityOne: '',
+			adjectiveTwo: '',
+			nounOne: '',
+			numberOne: '',
+			numberTwo: '',
+			nounTwo: '',
+			adjectiveThree: '',
+			celebrityTwo: '',
+			celebrityThree: '',
+			adjectiveFour: '',
+			nounThree: '',
+			celebrityFour: '',
+			adjectiveFive: ''
 		};
 
 		this.handleInputChange = this.handleInputChange.bind(this);
@@ -21,11 +35,32 @@ class Card extends Component {
 	}
 
 	render() {
+		const inputData = [
+			{ title: 'Color', state: this.state.color, name: 'color' },
+			{ title: 'Plural Noun', state: this.state.pluralNoun, name: 'pluralNoun' },
+			{ title: 'Adjective', state: this.state.adjectiveOne, name: 'adjectiveOne' },
+			{ title: 'Celebrity', state: this.state.celebrityOne, name: 'celebrityOne' },
+
+			{ title: 'Adjective', state: this.state.adjectiveTwo, name: 'adjectiveTwo' },
+			{ title: 'Noun', state: this.state.nounOne, name: 'nounOne' },
+			{ title: 'Number', state: this.state.numberOne, name: 'numberOne' },
+			{ title: 'Number', state: this.state.numberTwo, name: 'numberTwo' },
+
+			{ title: 'Noun', state: this.state.nounTwo, name: 'nounTwo' },
+			{ title: 'Adjective', state: this.state.adjectiveThree, name: 'adjectiveThree' },
+			{ title: 'Celebrity', state: this.state.celebrityTwo, name: 'celebrityTwo' },
+			{ title: 'Celebrity', state: this.state.celebrityThree, name: 'celebrityThree' },
+
+			{ title: 'Adjective', state: this.state.adjectiveFour, name: 'adjectiveFour' },
+			{ title: 'Noun', state: this.state.nounThree, name: 'nounThree' },
+			{ title: 'Celebrity', state: this.state.celebrityFour, name: 'celebrityFour' },
+			{ title: 'Adjective', state: this.state.adjectiveFive, name: 'adjectiveFive' },
+		];
 		return (
 			<div className="card">
-			<h1>{ this.state.color }</h1>
-			{ Input('Color', this.state.color, this.handleInputChange, 'color' ) }
-			{ Input('Plural Noun', this.state.pluralNoun, this.handleInputChange, 'pluralNoun' ) }
+				{
+					inputData.map(data => Input((data), this.handleInputChange)) // unsure why parenthesis were needed around "data"
+				}
 			</div>
 		);
 	}
