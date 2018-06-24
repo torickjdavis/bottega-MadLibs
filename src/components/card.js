0e1d09cbd93518a86c2531ca8e012ff55e676e5f
Torick Davis <torickjdavis@gmail.com>
2018-06-24 00:03:23 +0000

    Finished clear form functionality. Created INITIAL_STATE const to remove redundant code.

--- src/components/card.js
+++ src/components/card.js
@@ -3,28 +3,30 @@ import React, { Component } from 'react';
 import Input from './input';
 import Content from './content';
 
+const INITIAL_STATE = {
+	color: '',
+	pluralNoun: '',
+	adjectiveOne: '',
+	celebrityOne: '',
+	adjectiveTwo: '',
+	nounOne: '',
+	numberOne: '',
+	numberTwo: '',
+	nounTwo: '',
+	adjectiveThree: '',
+	celebrityTwo: '',
+	celebrityThree: '',
+	adjectiveFour: '',
+	nounThree: '',
+	celebrityFour: '',
+	adjectiveFive: '',
+	contentVisible: false
+};
+
 class Card extends Component {
 	constructor() {
 		super();
-		this.state = {
-			color: '',
-			pluralNoun: '',
-			adjectiveOne: '',
-			celebrityOne: '',
-			adjectiveTwo: '',
-			nounOne: '',
-			numberOne: '',
-			numberTwo: '',
-			nounTwo: '',
-			adjectiveThree: '',
-			celebrityTwo: '',
-			celebrityThree: '',
-			adjectiveFour: '',
-			nounThree: '',
-			celebrityFour: '',
-			adjectiveFive: '',
-			contentVisible: false
-		};
+		this.state = INITIAL_STATE;
 
 		this.handleInputChange = this.handleInputChange.bind(this);
 		this.handleFormSubmit = this.handleFormSubmit.bind(this);
@@ -38,7 +40,12 @@ class Card extends Component {
 
 	handleFormSubmit(event) {
 		event.preventDefault();
-		this.setState({ contentVisible: !this.state.contentVisible });
+		if (this.state.contentVisible) {
+			this.setState(INITIAL_STATE);
+		}
+		else {
+			this.setState({ contentVisible: true });
+		}
 	}
 
 	render() {
